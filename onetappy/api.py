7c69d94e1d6fc1a6c23649ac7fa148478f7c7680
aaronlyy <aaronlevican@gmail.com>
2021-03-16 16:42:12 +0100

    added basic structure

---
+++ onetappy/api.py
@@ -0,0 +1,153 @@
+# api.py
+# https://www.onetap.com/threads/cloud-api.37180/
+# Author: Aaron Levi (aaronlyy)
+# Repo: https://github.com/aaronlyy/onetappy
+# v0.1
+
+import requests
+
+# TODO:
+# add methods: 
+
+# region base class
+class Onetappy:
+    """GET, POST or DELETE configs from/to the cloud.
+    """
+
+    def __init__(self, api_id:str, api_secret:str, api_key:str):
+        self._headers = {
+            "X-Api-Id": api_id,
+            "X-Api-Secret": api_secret,
+            "X-Api-Key": api_key
+        }
+
+        self._base_url = "https://api.onetap.com/cloud/"
+
+# region base request method
+    def _req(self, endpoint:str, method:str, data:dict=None):
+        url = f"{self._base_url}{endpoint}"
+
+        if method == "GET":
+            return requests.get(url, headers=self._headers)
+        elif method == "POST":
+            return requests.post(url, headers=self._headers, data=data)
+        elif method == "DELETE":
+            pass
+        else:
+            raise Exception
+# endregion
+
+# region configs
+    def get_configs(self):
+        """Gets all available configs
+        """
+        endpoint = "configs/"
+        res = self._req(endpoint, "GET")
+        return OnetappyResponse(res)
+
+    def get_config(self, config_id:str):
+        """Gets a specific config.
+
+        Args:
+            config_id (str): ID of the config
+        """
+        endpoint = f"configs/{config_id}"
+        res = self._req(endpoint, "GET")
+        return OnetappyResponse(res)
+
+    def create_config(self, name, data:str):
+        pass
+
+    def update_config(self, config_id, name:str, data:str):
+        pass
+
+    def delete_config(self, config_id):
+        pass
+# endregion
+
+# region config invites
+    def get_config_invites(self):
+        pass
+
+    def get_config_invite(self, config_id):
+        pass
+
+    def create_config_invite(self, config_id, max_age:int, max_uses:int):
+        pass
+
+    def delete_config_invite(self, config_id, invite_id:int):
+        pass
+# endregion
+
+# region config subscription
+    def get_all_config_subs(self):
+        pass
+
+    def get_config_subs(self, config_id):
+        pass
+
+    def create_config_sub(self, config_id, user_id:int):
+        pass
+
+    def delete_config_sub(self, config_id, user_id:int):
+        pass
+# endregion
+
+# region scripts
+    def get_scripts(self):
+        pass
+    
+    def get_script(self, script_id):
+        pass
+
+    def update_script(self, script_id, name:str):
+        pass
+
+    def delete_script(self, script_id):
+        pass
+# endregion
+
+# region script invite
+    def get_all_script_invites(self):
+        pass
+
+    def get_script_invites(self, script_id):
+        pass
+
+    def create_script_invite(self, script_id, max_age:int, max_uses:int):
+        pass
+
+    def delete_script_invite(self, script_id, invite_id:int):
+        pass
+# endregion
+
+# region script subscriptions
+    def get_all_script_subs(self):
+        pass
+    
+    def get_script_subs(self, script_id):
+        pass
+
+    def create_script_sub(self, script_id, user_id:int):
+        pass
+
+    def delete_script_sub(self, script_id, user_id:int):
+        pass
+# endregion
+
+# endregion
+
+# region response class
+class OnetappyResponse:
+    def __init__(self, res):
+        self._json = res.json()
+
+    def json(self):
+        return self._json
+
+    def __getattr__(self, attr):
+        if attr in self._json:
+            return attr
+        else:
+            return None
+# endregion
